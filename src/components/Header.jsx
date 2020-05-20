0b318326659fbdec48c2298225756e3b9369cce9
roy <roy@gmail>
2020-05-20 13:45:40 +0530

    logo not displaying while deploying in git pages..trying a fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Summary from "./Summary";
-
+import logo from "../images/covid.png";
 export default function Header() {
-  const imgUrl = "/images/covid.png";
+  //  const imgUrl = "/images/covid.png";
 
   return (
     <div
       className="jumbotron "
       style={{
-        background: "url(" + imgUrl + ")",
+        background: "url(" + logo + ")",
         backgroundSize: "contain",
         backgroundPosition: "center",
       }}
